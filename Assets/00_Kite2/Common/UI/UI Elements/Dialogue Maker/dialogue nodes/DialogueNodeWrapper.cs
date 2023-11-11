7e7f966e42aed3dfb6d4c73e636b81c98ef50a99
Mergim Miftari <mmiftari@stud.hs-heilbronn.de>
2023-11-11 23:02:41 +0100

    Made Context Menu of dialogue Node work.

--- Assets/00_Kite2/Common/UI/UI Elements/Dialogue Maker/dialogue nodes/DialogueNodeWrapper.cs
+++ Assets/00_Kite2/Common/UI/UI Elements/Dialogue Maker/dialogue nodes/DialogueNodeWrapper.cs
@@ -8,11 +8,49 @@ public class DialogueNodeWrapper : MonoBehaviour
     [SerializeField] private RectTransform dialogueNodeWrapperRectTransform;
     [SerializeField] private RectTransform dialogueNodeRectTransform;
     [SerializeField] private RectTransform contextMenuRectTransform;
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button addNodeButton;
+    [SerializeField] private Button editNodeButton;
+    [SerializeField] private Button connectNodeButton;
+    [SerializeField] private bool isContextMenuVisible;
+    [SerializeField] private GameObject contextMenu;
 
     void Start()
     {
         LayoutRebuilder.ForceRebuildLayoutImmediate(dialogueNodeWrapperRectTransform);
         LayoutRebuilder.ForceRebuildLayoutImmediate(dialogueNodeRectTransform);
         LayoutRebuilder.ForceRebuildLayoutImmediate(contextMenuRectTransform);
+
+        optionsButton.onClick.AddListener(delegate { OnOptionsButton(); });
+        addNodeButton.onClick.AddListener(delegate { OnAddNodeButton(); });
+        editNodeButton.onClick.AddListener(delegate { OnEditNodeButton(); });
+        connectNodeButton.onClick.AddListener(delegate { OnConnectNodesButton(); });
+    }
+
+    public void OnOptionsButton()
+    {
+        if (isContextMenuVisible)
+        {
+            isContextMenuVisible = false;
+            contextMenu.SetActive(false);
+            return;
+        }
+        isContextMenuVisible = true;
+        contextMenu.SetActive(true);
+    }
+
+    public void OnAddNodeButton()
+    {
+        Debug.Log("Add Node Button");
+    }
+
+    public void OnEditNodeButton()
+    {
+        Debug.Log("Edit Node Button");
+    }
+
+    public void OnConnectNodesButton()
+    {
+        Debug.Log("Connect Node Button");
     }
 }
