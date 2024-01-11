06e51deca0eb52d5a41859b133176dbb32c1d583
Mergim Miftari <mmiftari@stud.hs-heilbronn.de>
2024-01-11 23:38:26 +0100

    Now it is possible to add Observers via the App.

--- Assets/00_Kite2/UserFeedback/AddObserverSceneController.cs
+++ Assets/00_Kite2/UserFeedback/AddObserverSceneController.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AddObserverSceneController : SceneController
+public class AddObserverSceneController : SceneController, OnSuccessHandler
 {
+    [SerializeField] private RectTransform layout;
+    [SerializeField] private Button confirmButton;
+    [SerializeField] private TMP_InputField inputField;
+    [SerializeField] private GameObject addObserverServerCallPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
+        LayoutRebuilder.ForceRebuildLayoutImmediate(layout);
         BackStackManager.Instance().Push(SceneNames.ADD_OBSERVER_SCENE);
+        confirmButton.onClick.AddListener(delegate { OnConfirmButton(); });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnConfirmButton()
     {
-        
+        if (string.IsNullOrEmpty(inputField.text.Trim()))
+        {
+            this.DisplayInfoMessage("Bitte Email-Adresse angeben!");
+            return;
+        }
+        AddReviewObserverServerCall call = Instantiate(addObserverServerCallPrefab).GetComponent<AddReviewObserverServerCall>();
+        call.sceneController = this;
+        call.onSuccessHandler = this;
+        call.email = inputField.text.Trim();
+        call.SendRequest();
+        DontDestroyOnLoad(call.gameObject);
+        inputField.text = "";
+    }
+
+    public void OnSuccess(Response response)
+    {
+        this.DisplayInfoMessage("Email-Adresse erfolgreich hinzugefügt!");
     }
 }
