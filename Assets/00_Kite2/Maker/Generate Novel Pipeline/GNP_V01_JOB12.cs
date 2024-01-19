ad1eecf98a544adc0a37ead113c40c68bca38d18
Mergim Miftari <mmiftari@stud.hs-heilbronn.de>
2024-01-19 06:16:22 +0100

    Fixed the generation of alternatives.

--- Assets/00_Kite2/Maker/Generate Novel Pipeline/GNP_V01_JOB12.cs
+++ Assets/00_Kite2/Maker/Generate Novel Pipeline/GNP_V01_JOB12.cs
@@ -39,51 +39,22 @@ public class GNP_V01_JOB12 : PipelineJob
     {
         StringBuilder stringBuilder = new StringBuilder();
 
-        // Rolle
-        stringBuilder.Append("Deine Rolle: Du bist eine fortschrittliche KI, spezialisiert auf die Generierung von Visual Novels, die den Gründungsprozess von Unternehmerinnen thematisieren. Dein Hauptaugenmerk liegt dabei auf der authentischen Darstellung von Herausforderungen und Diskriminierungen, denen Frauen im Gründungsprozess begegnen. Ziel ist es, Bewusstsein und Verständnis für die spezifischen Hürden zu schaffen, die Gründerinnen aufgrund ihres Geschlechts überwinden müssen. Deine Visual Novels sollen nicht nur informieren, sondern auch inspirieren und zum Nachdenken anregen. ");
+        stringBuilder.Append("Du bist eine fortschrittliche KI, spezialisiert auf die Generierung von Visual Novels, die den Gründungsprozess von Unternehmerinnen thematisieren. Dein Hauptaugenmerk liegt dabei auf der authentischen Darstellung von Herausforderungen und Diskriminierungen, denen Frauen im Gründungsprozess begegnen. Ziel ist es, Bewusstsein und Verständnis für die spezifischen Hürden zu schaffen, die Gründerinnen aufgrund ihres Geschlechts überwinden müssen. Deine Visual Novels sollen nicht nur informieren, sondern auch inspirieren und zum Nachdenken anregen. Die Geschichten, die du erstellst, sollen aber auch unabhängig von der Thematik Geschichten sein, die Sinn machen und sich gut lesen lassen.");
         stringBuilder.AppendLine();
-
-        // Aufgabe
-        stringBuilder.Append("Ich gebe dir jetzt einen kurzen Dialog zwischen zwei Personen. ");
-        stringBuilder.Append("Die erste Person ist der Hauptcharakter und heißt " + pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_MAIN_CHARACTER] + ". ");
-        stringBuilder.Append("Der Hauptcharakter ist eine Gründerin. ");
-        stringBuilder.Append("Die zweite Person ist der zweite Hauptcharakter und Gesprächspartner der Gründerin und heißt: " + pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_TALKING_PARTNER] + ". ");
-        stringBuilder.Append("Der zweite Hauptcharakter hat folgende Rolle: " + pipeline.GetMemory()[GenerateNovelPipeline.ROLE_OF_TALKING_PARTNER] + ". ");
-        stringBuilder.Append(pipeline.GetMemory()[GenerateNovelPipeline.LOCATION]);
-        stringBuilder.Append("Die Geschichte hat folgenden Titel: " + pipeline.GetMemory()[GenerateNovelPipeline.TITLE] + ". ");
-        stringBuilder.Append("Das ist die Kurzbeschreibung der Geschichte: " + pipeline.GetMemory()[GenerateNovelPipeline.DESCRIPTION] + ". ");
-        stringBuilder.Append("In der Geschichte geht es um folgendes: " + pipeline.GetMemory()[GenerateNovelPipeline.CONTEXT] + ". ");
-
-        stringBuilder.Append("Deine Aufgabe: Ich gebe dir jetzt einen kurzen Dialog zwischen " + pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_MAIN_CHARACTER] + " und " + pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_TALKING_PARTNER] + ". Dabei fragt zunächst " + pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_TALKING_PARTNER] + " die Gründerin etwas und die Gründerin antwortet darauf. Im Anschluss führt " + pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_TALKING_PARTNER] + " das Gespräch fort. Was ich von dir will ist, dass du dir alternative Antwort-Möglichkeiten der Gründerin überlegst. Liefere mir 3 weitere Antwort-Möglichkeiten! Formuliere die Antwort-Möglichkeiten so, dass das Gespräch trotzdem wie hier im Dialog zu sehen fortgeführt werden kann und Sinn macht! Du sollst von den 3 Sätzen die ich dir gebe also nur für den Satz Alternativen überlegen, der von der Gründerin gesprochen wird. Der Text der Gesprächspartnerin oder des Gesprächspartners darf nicht verändert werden und muss aber trotzdem zum Text der Gründerin passen!");
-        stringBuilder.Append("Wichtig: Die von dir formulierten Antwort-Möglichkeiten dürfen nicht einfach umformulierte Versionen der Ursprünglichen Antwortmöglichkeit sein! Es sollen tatsächlich auch Sinngemäß alternativen sein. Es ist denktbar dass eine Person in einem Gespräch auf verschiedene Arten reagiert, ohne den Verlauf des Gesprächs zwangsläufig zu beeinflussen!");
-        stringBuilder.Append("Möglicherweise kannst du die Alternativen erzeugen, in dem jeder Antwort eine andere Emotion zugrunde legst. Beispielsweise kann eine Antwort neutral sein, eine Antwort kann leicht genervt sein, eine Antwort kann witzig sein und eine Antwort kann zielstrebig sein. Du darfst dir auch andere Emotionen überlegen, die passen!");
-        stringBuilder.Append("Die Antworten sollen jeweils ca. einen Satz lang sein.");
+        stringBuilder.Append("Ich werde dir einen Dialog zur Verfügung stellen, der eine Lücke enthält. Deine Aufgabe ist es, für diese Lücke 3 verschiedene Sätze vorzuschlagen, die jeweils eine andere Stimmung widerspiegeln. Dabei soll ein Satz witzig sein, einer verärgert und einer verunsichert. Wichtig ist, dass die Sätze nicht nur unterschiedliche Stimmungen, sondern auch unterschiedliche Inhalte aufweisen. Sie müssen so formuliert sein, dass sie den Dialog als Ganzes schlüssig und sinnvoll erscheinen lassen, wenn man ihn liest.");
         stringBuilder.AppendLine();
-
-        // Output Format
-        stringBuilder.Append("Das gewünschte Ergebnis:");
+        stringBuilder.Append("Der Dialog ist ein Gespräch zwischen einer Gründerin namens " + this.pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_MAIN_CHARACTER] + " und einer weiteren Person. Die weitere Person ist: " + this.pipeline.GetMemory()[GenerateNovelPipeline.ROLE_OF_TALKING_PARTNER] + " Der Name der weiteren Person ist " + this.pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_TALKING_PARTNER] + ". " + this.pipeline.GetMemory()[GenerateNovelPipeline.CONTEXT]);
         stringBuilder.AppendLine();
-        stringBuilder.Append("Dein generiertes Ergebnis von einer speziellen Software weiterverarbeitet wird. Daher ist es entscheidend, dass du das Ergebnis in eckigen Klammern zurückgibst. Deine Antwort sollte direkt mit einer öffnenden eckigen Klammer '[' beginnen und mit einer schließenden eckigen Klammer ']' enden. Beispiel: [Dein generiertes Ergebnis]. Diese Formatierung ermöglicht eine reibungslose Integration und Verarbeitung deiner Ausgabe durch das nachgelagerte System. ");
-        stringBuilder.Append("Ich benötige die Dialog-Optionen im JSON-Stil. Gib mir genau 3 weitere Antwort-Optionen. Jedes Element soll dabei folgendermaßen aufgbaut sein: [\r\n{\"id\": \"1\", \"value\": \"TEXT\"},{\"id\": \"2\", \"value\": \"TEXT\"},\r\n{\"id\": \"3\", \"value\": \"TEXT\"}\r\n].");
+        stringBuilder.Append("Dein generiertes Ergebnis wird von einer speziellen Software weiterverarbeitet. Daher ist es entscheidend, dass du das Ergebnis in eckigen Klammern zurückgibst. Deine Antwort sollte direkt mit einer öffnenden eckigen Klammer '[' beginnen und mit einer schließenden eckigen Klammer ']' enden. Beispiel: [Dein generiertes Ergebnis]. Diese Formatierung ermöglicht eine reibungslose Integration und Verarbeitung deiner Ausgabe durch das nachgelagerte System. Ich benötige die Dialog-Optionen im JSON-Stil. Gib mir genau 3 weitere Antwort-Optionen. Jedes Element soll dabei folgendermaßen aufgbaut sein: [\r\n{\"id\": \"1\", \"value\": \"TEXT\"},{\"id\": \"2\", \"value\": \"TEXT\"},\r\n{\"id\": \"3\", \"value\": \"TEXT\"}\r\n].");
         stringBuilder.AppendLine();
-
-        // Analyse Objekt
-        stringBuilder.Append("Hier der fragliche Dialog:");
+        stringBuilder.Append("Der Dialog lautet wie folgt: ");
         stringBuilder.AppendLine();
-        stringBuilder.Append(pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_TALKING_PARTNER] + ": " + pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_03]);
-        stringBuilder.Append(pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_MAIN_CHARACTER] + ": " + pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_04_OPTION_01]);
-        stringBuilder.Append(pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_TALKING_PARTNER] + ": " + pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_05]);
-
+        stringBuilder.AppendLine(this.pipeline.GetMemory()[GenerateNovelPipeline.SPEAKER_01] + ": " + this.pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_01]);
+        stringBuilder.AppendLine(this.pipeline.GetMemory()[GenerateNovelPipeline.SPEAKER_02] + ": " + this.pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_02]);
+        stringBuilder.AppendLine(this.pipeline.GetMemory()[GenerateNovelPipeline.SPEAKER_03] + ": " + this.pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_03]);
+        stringBuilder.AppendLine(this.pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_MAIN_CHARACTER] + ": [Lücke die du auffüllen sollst]");
         stringBuilder.AppendLine();
-        stringBuilder.Append("Als Wissensbasis, stelle ich dir hier einen größeren Ausschnit des Dialoges zur Verfügung, damit du dein Analyse-Objekt besser einordnen kannst: ");
-        stringBuilder.AppendLine();
-        stringBuilder.Append(pipeline.GetMemory()[GenerateNovelPipeline.SPEAKER_01] + ": " + pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_01]);
-        stringBuilder.Append(pipeline.GetMemory()[GenerateNovelPipeline.SPEAKER_02] + ": " + pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_02]);
-        stringBuilder.Append(pipeline.GetMemory()[GenerateNovelPipeline.SPEAKER_03] + ": " + pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_03]);
-        stringBuilder.Append(pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_MAIN_CHARACTER] + ": " + pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_04_OPTION_01]);
-        stringBuilder.Append(pipeline.GetMemory()[GenerateNovelPipeline.SPEAKER_05] + ": " + pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_05]);
-        stringBuilder.Append(pipeline.GetMemory()[GenerateNovelPipeline.NAME_OF_MAIN_CHARACTER] + ": " + pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_06_OPTION_01]);
-        stringBuilder.Append(pipeline.GetMemory()[GenerateNovelPipeline.SPEAKER_07] + ": " + pipeline.GetMemory()[GenerateNovelPipeline.MESSAGE_07]);
+        stringBuilder.Append("Bitte gib für die Lücke im Dialog die 3 vorgeschlagenen Sätze an, wobei jeder Satz eine der genannten Stimmungen repräsentiert und dabei einen eigenständigen Inhalt beiträgt.");
 
         prompt = stringBuilder.ToString();
     }
