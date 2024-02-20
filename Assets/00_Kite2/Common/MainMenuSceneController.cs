866707f8ccbf16f4d372fc500aee95b4c599fb4a
Mergim Miftari <mmiftari@stud.hs-heilbronn.de>
2024-02-20 12:17:30 +0100

    Added terms of use, imprint and data privacy information to the client.

--- Assets/00_Kite2/Common/MainMenuSceneController.cs
+++ Assets/00_Kite2/Common/MainMenuSceneController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using Unity.Services.Analytics;
 
 public class MainMenuSceneController : SceneController, OnSuccessHandler
 { 
