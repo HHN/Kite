b0729a9700205cba41530d8be0073a30feafff3f
Mergim Miftari <Mergim-Miftari@hotmail.de>
2024-06-03 14:11:44 +0200

    Finished the new pages

--- Assets/00_Kite2/NewPages/BarrierefreiheitSceneController.cs
+++ Assets/00_Kite2/NewPages/BarrierefreiheitSceneController.cs
@@ -1,11 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BarrierefreiheitSceneController : SceneController
 {
+    [SerializeField] private Toggle toggleTextToSpeech;
+    [SerializeField] private Button toggleTextToSpeechInfoButton;
+    [SerializeField] private AudioSource audioSource;
+
     void Start()
     {
         BackStackManager.Instance().Push(SceneNames.BARRIEREFREIHEIT_SCENE);
+
+        InitializeToggleTextToSpeech();
+
+        toggleTextToSpeech.onValueChanged.AddListener(delegate { OnToggleTextToSpeech(toggleTextToSpeech); });
+        toggleTextToSpeechInfoButton.onClick.AddListener(delegate { OnToggleTextToSpeechInfoButton(); });
+    }
+
+    public void InitializeToggleTextToSpeech()
+    {
+        if (TextToSpeechManager.Instance().IsTextToSpeechActivated())
+        {
+            toggleTextToSpeech.isOn = true;
+        }
+        else
+        {
+            toggleTextToSpeech.isOn = false;
+        }
+    }    
+    
+    public void OnToggleTextToSpeech(Toggle toggle)
+    {
+        if (toggleTextToSpeech.isOn)
+        {
+            TextToSpeechManager.Instance().ActivateTextToSpeech();
+            DisplayInfoMessage(InfoMessages.STARTED_TOGGLETEXTTOSPEECH_BUTTON);
+            TextToSpeechService.Instance().TextToSpeech("textWirdVorgelesen");
+        }
+        else
+        {
+            TextToSpeechManager.Instance().DeactivateTextToSpeech();
+            DisplayInfoMessage(InfoMessages.STOPPED_TOGGLETEXTTOSPEECH_BUTTON);
+            TextToSpeechService.Instance().TextToSpeech("textWirdNichtMehrVorgelesen", true);
+        }
+    }
+
+    public void OnToggleTextToSpeechInfoButton()
+    {
+        TextToSpeechService.Instance().TextToSpeech("textToSpeechInfo");
+        DisplayInfoMessage(InfoMessages.EXPLANATION_TEXTTOSPEECH_BUTTON);
     }
 }
