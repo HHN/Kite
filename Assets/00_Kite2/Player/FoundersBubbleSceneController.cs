7da4a78a2fac82bc0fdeb17f7b447c935770b75e
Mergim Miftari <Mergim-Miftari@hotmail.de>
2024-06-14 15:46:10 +0200

    Imported Novel Gespräch mit Bekanntem

--- Assets/00_Kite2/Player/FoundersBubbleSceneController.cs
+++ Assets/00_Kite2/Player/FoundersBubbleSceneController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking.Types;
 using UnityEngine.UI;
 
 public class FoundersBubbleSceneController : SceneController
