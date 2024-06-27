72832f0c9e8bb9b526f21ccaeff8d6c1bbcdd458
Mergim Miftari <Mergim-Miftari@hotmail.de>
2024-06-27 11:37:03 +0200

    Prepared for Deployment

--- Assets/00_Kite2/Common/MainMenuSceneController.cs
+++ Assets/00_Kite2/Common/MainMenuSceneController.cs
@@ -16,7 +16,7 @@ public class MainMenuSceneController : SceneController, OnSuccessHandler
     [SerializeField] private TextMeshProUGUI infoTextTermsAndConditions;
     [SerializeField] private AudioSource kiteAudioLogo;
     [SerializeField] private GameObject getVersionServerCallPrefab;
-    [SerializeField] private static int COMPATIBLE_SERVER_VERSION_NUMBER = 9;
+    [SerializeField] private static int COMPATIBLE_SERVER_VERSION_NUMBER = 10;
     [SerializeField] private GameObject novelLoader;
 
     void Start()
