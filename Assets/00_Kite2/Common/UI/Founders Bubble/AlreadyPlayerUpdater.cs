8e566dcb804429c5b7e5a8599ca35d06a395a066
Mergim Miftari <Mergim-Miftari@hotmail.de>
2024-08-28 16:11:08 +0200

    Fixed bug

--- Assets/00_Kite2/Common/UI/Founders Bubble/AlreadyPlayerUpdater.cs
+++ Assets/00_Kite2/Common/UI/Founders Bubble/AlreadyPlayerUpdater.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 
 public class AlreadyPlayerUpdater : MonoBehaviour
 {
