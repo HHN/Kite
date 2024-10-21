4486c8df333aba41ca4ef60525625412464bbbd8
S3nnar <privat.mc@t-online.de>
2024-10-21 18:34:00 +0200

    increased version number

--- Assets/00_Kite2/Common/MainMenuSceneController.cs
+++ Assets/00_Kite2/Common/MainMenuSceneController.cs
@@ -14,7 +14,7 @@ public class MainMenuSceneController : SceneController, OnSuccessHandler
     [SerializeField] private TextMeshProUGUI infoTextTermsAndConditions;
     [SerializeField] private AudioSource kiteAudioLogo;
     [SerializeField] private GameObject getVersionServerCallPrefab;
-    [SerializeField] private static int COMPATIBLE_SERVER_VERSION_NUMBER = 3;
+    [SerializeField] private static int COMPATIBLE_SERVER_VERSION_NUMBER = 4;
     [SerializeField] private GameObject novelLoader;
 
     private void Start()
