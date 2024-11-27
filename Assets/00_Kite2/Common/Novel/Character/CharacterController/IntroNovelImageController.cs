7f344819f70dfc2c35fd216cca700d6a76f05783
RyanAella <72981983+RyanAella@users.noreply.github.com>
2024-11-27 15:17:30 +0100

    Standardization of the fonts used

--- Assets/00_Kite2/Common/Novel/Character/CharacterController/IntroNovelImageController.cs
+++ Assets/00_Kite2/Common/Novel/Character/CharacterController/IntroNovelImageController.cs
@@ -1,171 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
-using _00_Kite2.Common.Novel.Character.CharacterController;
-using UnityEngine.UI;
-using CharacterController = _00_Kite2.Common.Novel.Character.CharacterController.CharacterController;
 
-public class IntroNovelImageController : NovelImageController
+namespace _00_Kite2.Common.Novel.Character.CharacterController
 {
-    [SerializeField] private GameObject decoGlasPrefab;
-    [SerializeField] private GameObject decoGlasContainer;
-    [SerializeField] private GameObject decoPlantPrefab;
-    [SerializeField] private GameObject decoPlantContainer;
-    [SerializeField] private GameObject characterPrefab;
-    [SerializeField] private AudioClip decoGlasAudio;
-    [SerializeField] private AudioClip decoPlantAudio;
-    [SerializeField] private Sprite[] animationFramesPlant;
-    [SerializeField] private Sprite[] animationFramesGlas;
-    private GameObject decoGlas = null;
-    private GameObject decoPlant = null;
-    private GameObject character = null;
-
-    void Start()
+    public class IntroNovelImageController : NovelImageController
     {
-        SetInitialSpirtesForImages();
-    }
-
-    private void SetInitialSpirtesForImages()
-    {
-
-        Image image = decoPlantPrefab.GetComponent<Image>();
-        image.sprite = animationFramesPlant[0];
-        if (decoPlantContainer.transform.childCount > 0)
+        [SerializeField] private GameObject characterPrefab;
+    
+        public override void SetCharacter()
         {
-            Destroy(decoPlantContainer.transform.GetChild(0).gameObject);
+            CharacterController = characterPrefab.GetComponent<CharacterController>();
         }
-        Instantiate(decoPlantPrefab, decoPlantContainer.transform);
 
-        image = decoGlasPrefab.GetComponent<Image>();
-        image.sprite = animationFramesGlas[0];
-        if (decoGlasContainer.transform.childCount > 0)
+        public override bool HandleTouchEvent(float x, float y, AudioSource audioSource)
         {
-            Destroy(decoGlasContainer.transform.GetChild(0).gameObject);
-        }
-        Instantiate(decoGlasPrefab, decoGlasContainer.transform);
-    }
-
-    public override void SetCharacter()
-    {
-        CharacterController = characterPrefab.GetComponent<CharacterController>();
-    }
-
-    public override bool HandleTouchEvent(float x, float y, AudioSource audioSource)
-    {
-        // Check if animations are allowed to proceed, return false if disabled
-        if (AnimationFlagSingleton.Instance().GetFlag() == false)
-        {
+            // Check if animations are allowed to proceed, return false if disabled
+            if (AnimationFlagSingleton.Instance().GetFlag() == false)
+            {
+                return false;
+            }
             return false;
         }
-
-        // Get the RectTransforms of the objects to detect touch within their bounds
-        RectTransform decoGlasRectTransform = decoGlasContainer.GetComponent<RectTransform>();
-        RectTransform decoPlantRectTransform = decoPlantContainer.GetComponent<RectTransform>();
-
-        // Get the world corners of the glass decoration container
-        Vector3[] cornersDecoDesk = new Vector3[4];
-        decoGlasRectTransform.GetWorldCorners(cornersDecoDesk);
-        Vector3 bottomLeftDecoDesk = cornersDecoDesk[0];
-        Vector3 topRightDecoDesk = cornersDecoDesk[2];
-
-        // Get the world corners of the plant decoration container
-        Vector3[] cornersDecoBackground = new Vector3[4];
-        decoPlantRectTransform.GetWorldCorners(cornersDecoBackground);
-        Vector3 bottomLeftDecoBackground = cornersDecoBackground[0];
-        Vector3 topRightDecoBackground = cornersDecoBackground[2];
-
-        // Check if the touch coordinates are within the glass decoration bounds
-        if (x >= bottomLeftDecoDesk.x && x <= topRightDecoDesk.x &&
-            y >= bottomLeftDecoDesk.y && y <= topRightDecoDesk.y)
-        {
-            StartCoroutine(OnDecoGlas(audioSource)); // Trigger the glass interaction
-            return true;
-        }
-        // Check if the touch coordinates are within the plant decoration bounds
-        else if (x >= bottomLeftDecoBackground.x && x <= topRightDecoBackground.x &&
-                 y >= bottomLeftDecoBackground.y && y <= topRightDecoBackground.y)
-        {
-            StartCoroutine(OnDecoPlant(audioSource)); // Trigger the plant interaction
-            return true;
-        }
-
-        // Return false if the touch is outside both bounds
-        return false;
-    }
-
-
-    private IEnumerator OnDecoPlant(AudioSource audioSource)
-    {
-        if (audioSource != null)
-        {
-            audioSource.clip = decoPlantAudio;
-            if (audioSource.clip != null)
-            {
-                audioSource.Play();
-                Image image = decoPlantPrefab.GetComponent<Image>();
-                image.sprite = animationFramesPlant[1];
-                Destroy(decoPlantContainer.transform.GetChild(0).gameObject);
-                Instantiate(decoPlantPrefab, decoPlantContainer.transform);
-                yield return new WaitForSeconds(0.5f);
-                image.sprite = animationFramesPlant[2];
-                Destroy(decoPlantContainer.transform.GetChild(0).gameObject);
-                Instantiate(decoPlantPrefab, decoPlantContainer.transform);
-                yield return new WaitForSeconds(0.5f);
-                image.sprite = animationFramesPlant[0];
-                Destroy(decoPlantContainer.transform.GetChild(0).gameObject);
-                Instantiate(decoPlantPrefab, decoPlantContainer.transform);
-            }
-            else
-            {
-                Debug.LogError("AudioClip couldn't be found.");
-            }
-        }
-        yield return new WaitForSeconds(0f);
-    }
-
-    private IEnumerator OnDecoGlas(AudioSource audioSource)
-    {
-        if (audioSource != null)
-        {
-            audioSource.clip = decoGlasAudio;
-            if (audioSource.clip != null)
-            {
-                audioSource.Play();
-                Image image = decoGlasPrefab.GetComponent<Image>();
-                image.sprite = animationFramesGlas[1];
-                Destroy(decoGlasContainer.transform.GetChild(0).gameObject);
-                Instantiate(decoGlasPrefab, decoGlasContainer.transform);
-                yield return new WaitForSeconds(0.5f);
-                image.sprite = animationFramesGlas[2];
-                Destroy(decoGlasContainer.transform.GetChild(0).gameObject);
-                Instantiate(decoGlasPrefab, decoGlasContainer.transform);
-                yield return new WaitForSeconds(0.5f);
-                image.sprite = animationFramesGlas[3];
-                Destroy(decoGlasContainer.transform.GetChild(0).gameObject);
-                Instantiate(decoGlasPrefab, decoGlasContainer.transform);
-                yield return new WaitForSeconds(0.5f);
-                image.sprite = animationFramesGlas[4];
-                Destroy(decoGlasContainer.transform.GetChild(0).gameObject);
-                Instantiate(decoGlasPrefab, decoGlasContainer.transform);
-                yield return new WaitForSeconds(0.5f);
-                image.sprite = animationFramesGlas[5];
-                Destroy(decoGlasContainer.transform.GetChild(0).gameObject);
-                Instantiate(decoGlasPrefab, decoGlasContainer.transform);
-                yield return new WaitForSeconds(0.5f);
-                image.sprite = animationFramesGlas[6];
-                Destroy(decoGlasContainer.transform.GetChild(0).gameObject);
-                Instantiate(decoGlasPrefab, decoGlasContainer.transform);
-                yield return new WaitForSeconds(0.5f);
-                image.sprite = animationFramesGlas[0];
-                Destroy(decoGlasContainer.transform.GetChild(0).gameObject);
-                Instantiate(decoGlasPrefab, decoGlasContainer.transform);
-            }
-            else
-            {
-                Debug.LogError("AudioClip couldn't be found.");
-            }
-        }
-        yield return new WaitForSeconds(0f);
     }
 }
--- Assets/00_Kite2/SaveNovelData/HintForSavegameMessageBox.cs
+++ Assets/00_Kite2/SaveNovelData/HintForSavegameMessageBox.cs
@@ -1,92 +1,95 @@
 using _00_Kite2.Player;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
-public class HintForSavegameMessageBox : MonoBehaviour
+namespace _00_Kite2.SaveNovelData
 {
-    [SerializeField] private TextMeshProUGUI messageBoxHeadline; // Text component for the headline of the message box
-    [SerializeField] private TextMeshProUGUI messageBoxBody; // Text component for the body of the message box
-    [SerializeField] private Button continueButton; // Button to continue with the novel
-    [SerializeField] private Button restartButton; // Button to restart the novel
-    [SerializeField] private GameObject textContinue; // GameObject containing the "Continue" text
-    [SerializeField] private GameObject textRestart; // GameObject containing the "Restart" text
-
-    [SerializeField]
-    private GameObject person; // GameObject representing the person or character related to the message
-
-    private PlayNovelSceneController
-        _playNovelSceneController; // Reference to the PlayNovelSceneController to manage novel actions
-
-    private void Awake()
+    public class HintForSavegameMessageBox : MonoBehaviour
     {
-        // Find and assign the PlayNovelSceneController component for novel control actions
-        _playNovelSceneController = FindObjectOfType<PlayNovelSceneController>();
-    }
+        [SerializeField] private TextMeshProUGUI messageBoxHeadline; // Text component for the headline of the message box
+        [SerializeField] private TextMeshProUGUI messageBoxBody; // Text component for the body of the message box
+        [SerializeField] private Button continueButton; // Button to continue with the novel
+        [SerializeField] private Button restartButton; // Button to restart the novel
+        [SerializeField] private GameObject textContinue; // GameObject containing the "Continue" text
+        [SerializeField] private GameObject textRestart; // GameObject containing the "Restart" text
 
-    void Start()
-    {
-        // Attach listeners to buttons to handle click events
-        continueButton.onClick.AddListener(OnContinueButton);
-        restartButton.onClick.AddListener(OnRestartButton);
+        [SerializeField]
+        private GameObject person; // GameObject representing the person or character related to the message
 
-        // Initialize UI settings
-        InitUI();
+        private PlayNovelSceneController
+            _playNovelSceneController; // Reference to the PlayNovelSceneController to manage novel actions
 
-        // Update all text components with the proper font size
-        FontSizeManager.Instance().UpdateAllTextComponents();
-    }
-
-    /// <summary>
-    /// Initialize the UI elements by setting up appropriate colors and other properties.
-    /// </summary>
-    private void InitUI()
-    {
-        // Retrieve the color from the NovelColorManager instance
-        Color color = NovelColorManager.Instance().GetColor();
-
-        // Set the color of the text components to match the selected novel color theme
-        textContinue.GetComponent<TextMeshProUGUI>().color = color;
-        textRestart.GetComponent<TextMeshProUGUI>().color = color;
-    }
-
-    /// <summary>
-    /// Activate the message box to make it visible on the screen.
-    /// </summary>
-    public void Activate()
-    {
-        this.gameObject.SetActive(true);
-    }
-
-    /// <summary>
-    /// Handle the event when the continue button is clicked.
-    /// </summary>
-    private void OnContinueButton()
-    {
-        _playNovelSceneController.ResumeFromSavedState();
-        CloseMessageBox();
-    }
-
-    /// <summary>
-    /// Handle the event when the restart button is clicked.
-    /// </summary>
-    private void OnRestartButton()
-    {
-        _playNovelSceneController.RestartNovel();
-        CloseMessageBox();
-    }
-
-    /// <summary>
-    /// Close and destroy the message box.
-    /// </summary>
-    public void CloseMessageBox()
-    {
-        // Ensure the object is not null or destroyed before attempting to destroy it
-        if (this.IsNullOrDestroyed() || this.gameObject.IsNullOrDestroyed())
+        private void Awake()
         {
-            return;
+            // Find and assign the PlayNovelSceneController component for novel control actions
+            _playNovelSceneController = FindObjectOfType<PlayNovelSceneController>();
         }
 
-        Destroy(this.gameObject);
+        void Start()
+        {
+            // Attach listeners to buttons to handle click events
+            continueButton.onClick.AddListener(OnContinueButton);
+            restartButton.onClick.AddListener(OnRestartButton);
+
+            // Initialize UI settings
+            InitUI();
+
+            // Update all text components with the proper font size
+            FontSizeManager.Instance().UpdateAllTextComponents();
+        }
+
+        /// <summary>
+        /// Initialize the UI elements by setting up appropriate colors and other properties.
+        /// </summary>
+        private void InitUI()
+        {
+            // Retrieve the color from the NovelColorManager instance
+            Color color = NovelColorManager.Instance().GetColor();
+
+            // Set the color of the text components to match the selected novel color theme
+            textContinue.GetComponent<TextMeshProUGUI>().color = color;
+            textRestart.GetComponent<TextMeshProUGUI>().color = color;
+        }
+
+        /// <summary>
+        /// Activate the message box to make it visible on the screen.
+        /// </summary>
+        public void Activate()
+        {
+            gameObject.SetActive(true);
+        }
+
+        /// <summary>
+        /// Handle the event when the continue button is clicked.
+        /// </summary>
+        private void OnContinueButton()
+        {
+            _playNovelSceneController.ResumeFromSavedState();
+            CloseMessageBox();
+        }
+
+        /// <summary>
+        /// Handle the event when the restart button is clicked.
+        /// </summary>
+        private void OnRestartButton()
+        {
+            _playNovelSceneController.RestartNovel();
+            CloseMessageBox();
+        }
+
+        /// <summary>
+        /// Close and destroy the message box.
+        /// </summary>
+        public void CloseMessageBox()
+        {
+            // Ensure the object is not null or destroyed before attempting to destroy it
+            if (this.IsNullOrDestroyed() || this.gameObject.IsNullOrDestroyed())
+            {
+                return;
+            }
+
+            DestroyImmediate(this.gameObject);
+        }
     }
 }
