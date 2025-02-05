bdb2649f893c39bc2a749a2a0c267e3355b2b09c
RyanAella <72981983+RyanAella@users.noreply.github.com>
2025-02-05 04:33:18 -0800

    Re-imported Honorar Novel (#128)

--- Assets/00_Kite2/Common/Novel/Character/CharacterController/NovelImageController.cs
+++ Assets/00_Kite2/Common/Novel/Character/CharacterController/NovelImageController.cs
@@ -33,7 +33,6 @@ namespace _00_Kite2.Common.Novel.Character.CharacterController
 
         public void SetFaceExpression(int characterId, int expressionType)
         {
-            Debug.Log("SetFaceExpression: " + characterId + " : " + expressionType);
             // case CharacterRole.NONE: { return 0; }
             // case CharacterRole.PLAYER: { return 1; }
             // case CharacterRole.INTRO: { return 2; }
@@ -49,7 +48,7 @@ namespace _00_Kite2.Common.Novel.Character.CharacterController
 
             if (novelKite2CharacterController == null)
             {
-                Debug.Log("novelKite2CharacterController is null");
+                // Debug.Log("novelKite2CharacterController is null");
             }
             else if (characterId == 0)
             {
