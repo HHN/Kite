879b8e065d45c0a879b8be134bfd3321802b7bf7
Rebecca Biebl <72981983+RyanAella@users.noreply.github.com>
2025-03-24 15:57:29 +0100

    Recreating Import Framework

---
+++ Assets/_Scripts/Utilities/FaceExpressions.cs
@@ -0,0 +1,19 @@
+namespace Assets._Scripts.Utilities
+{
+    public enum FaceExpressions
+    {
+        NeutralRelaxed,
+        Rejecting,
+        Frightened,
+        Astonished,
+        Questioning,
+        Annoyed,
+        Critical,
+        Laughing,
+        LaughingBig,
+        Smiling,
+        Neutral,
+        Proud,
+        Unsatisfied
+    }
+}
