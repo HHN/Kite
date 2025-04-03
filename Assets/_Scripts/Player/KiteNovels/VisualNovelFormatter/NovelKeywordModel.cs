073d6eac6c62ec2bb8d14e0a08ffd76d362cee7e
S3nnar <privat.mc@t-online.de>
2025-04-03 13:10:01 +0200

    added NovelKeyWordModel and -Parser

---
+++ Assets/_Scripts/Player/KiteNovels/VisualNovelFormatter/NovelKeywordModel.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Assets
+{
+    public class NovelKeywordModel
+    {
+        public int? CharacterIndex { get; set; }
+        public string Action { get; set; }
+        public string FaceExpression { get; set; }
+        public string Bias { get; set; }
+        public string Sound { get; set; }
+        public bool? End { get; set; }
+    }
+
+}
